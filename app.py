f1514226acfd72d7b32d58975db6abfe2d8d0424
Pedro Ríos <pedroriosg@uc.cl>
2024-10-28 23:58:04 -0300

    feat: school ready

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ elif menu_selection == "Colegios":
     years_names = years_data['name'].tolist()
 
     # Crear columnas para los selectores
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     # Selector de colegios en la primera columna
     with col1:
@@ -69,27 +69,27 @@ elif menu_selection == "Colegios":
     with col2:
         selected_year = st.selectbox("Selecciona un año", years_names)
 
-    # Mostrar información del colegio seleccionado
+    # Obtener cursos del colegio seleccionado y el año
     if selected_school and selected_year:
-        # Obtener cursos del colegio seleccionado y el año
         courses_data = fetch_courses_of_school(selected_school, selected_year)
 
-        # Mostrar cursos como selector
-        if not courses_data.empty:
-            # Obtener los nombres de los cursos para el selector
-            courses_list = courses_data['grade_name'] + " - " + courses_data['subject_name']
-            selected_course = st.selectbox("Selecciona un curso", courses_list)
+        # Mostrar cursos como selector en la tercera columna
+        with col3:
+            if not courses_data.empty:
+                # Obtener los nombres de los cursos para el selector
+                courses_list = courses_data['grade_name'] + " - " + courses_data['subject_name']
+                selected_course = st.selectbox("Selecciona un curso", courses_list)
 
-            # Obtener el ID del curso seleccionado
-            selected_course_id = courses_data[courses_list == selected_course]['course_id'].values[0]
+                # Obtener el ID del curso seleccionado
+                selected_course_id = courses_data[courses_list == selected_course]['course_id'].values[0]
 
-            # Mostrar información del curso seleccionado
-            if selected_course_id is not None:
-                course_info = courses_data[courses_data['course_id'] == selected_course_id]
-                if not course_info.empty:
-
+        # Mostrar información del curso seleccionado
+        if selected_course_id is not None:
+            course_info = courses_data[courses_data['course_id'] == selected_course_id]
+            if not course_info.empty:
+                with st.expander("Usuarios", expanded=False):
                     # Filtro de rol para los usuarios de este curso
-                    role_filter = st.selectbox("Selecciona el rol de los usuarios", ["Todos", "Profesores", "Alumnos"])
+                    role_filter = st.selectbox("", ["Todos", "Profesores", "Alumnos"])
 
                     # Filtra los datos basándote en el rol seleccionado
                     user_data = fetch_users_by_course(selected_course_id)
@@ -106,19 +106,33 @@ elif menu_selection == "Colegios":
                     # Seleccionar solo las columnas que deseas mostrar en el DataFrame
                     filtered_data_display = user_data[columns_to_display].copy()
 
-                    # Convertir la columna birthday a datetime y luego formatearla
-                    # filtered_data_display['birthday'] = pd.to_datetime(filtered_data_display['birthday']).dt.date
-
-                    # Ajustar el ancho de las columnas
-                    column_widths = [100] * len(columns_to_display)  # Ancho fijo para todas las columnas
-
                     # Mostrar la tabla con ancho de columnas ajustado
-                    st.dataframe(filtered_data_display.style.set_properties(**{'width': f'{column_widths[0]}px'}), use_container_width=True)
+                    st.dataframe(filtered_data_display, use_container_width=True)
 
                     # Llamada a la función que muestra los gráficos de usuarios para este curso
                     display_user_charts(user_data)
                     if role_filter == "Profesores":
                         # Llamada a la función que muestra la distribución de universidades y carreras
                         display_user_education(user_data)
-        else:
-            st.write("No hay cursos disponibles para este colegio y año.")
+
+                    # Desplegable para mostrar información de los usuarios
+                    
+                # Desplegable para la asistencia
+                with st.expander("Asistencia", expanded=False):
+                    # attendance_data = fetch_attendance(selected_course_id)
+                    # if attendance_data is not None and not attendance_data.empty:
+                    #     st.dataframe(attendance_data)
+                    # else:
+                    #     st.write("No hay datos de asistencia disponibles.")
+                    pass
+
+                # Desplegable para evaluaciones
+                with st.expander("Evaluaciones", expanded=False):
+                    # evaluations_data = fetch_evaluations(selected_course_id)
+                    # if evaluations_data is not None and not evaluations_data.empty:
+                    #     st.dataframe(evaluations_data)
+                    # else:
+                    #     st.write("No hay datos de evaluaciones disponibles.")
+                    pass
+    else:
+        st.write("No hay cursos disponibles para este colegio y año.")
