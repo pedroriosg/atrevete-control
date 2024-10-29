9523b4135a325d543a9b0006a21399a0856c169f
Pedro Ríos <pedroriosg@uc.cl>
2024-10-28 23:29:01 -0300

    feat: school info

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from queries import fetch_users, fetch_attendance, fetch_evaluations, fetch_other_data
+from queries import fetch_users, fetch_schools, fetch_courses_of_school, fetch_years, fetch_users_by_course
 from views.users.user_table import display_user_table
 from views.users.user_charts import display_user_charts
 from views.users.user_education import display_user_education
@@ -9,15 +9,12 @@ import pandas as pd
 st.set_page_config(page_title="Panel de Control - Fundación Educacional Atrévete", layout="wide")
 
 # Título y estilo del Dashboard
-# Título y estilo del Dashboard
 st.title("Fundación Educacional Atrévete")
 st.markdown("---")
 
 menu_options = {
-    "Usuarios": "👥 Usuarios",
-    "Asistencia": "📅 Asistencia",
-    "Evaluaciones": "📝 Evaluaciones",
-    "Otros": "📋 Otros"
+    "Colegios": "🏫 Colegios",
+    "Usuarios": "👥 Usuarios"
 }
 menu_selection = st.sidebar.selectbox("", list(menu_options.keys()), format_func=lambda x: menu_options[x])
 
@@ -53,3 +50,75 @@ if menu_selection == "Usuarios":
     if role_filter == "Profesores":
         # Llamada a la función que muestra la distribución de universidades y carreras
         display_user_education(user_data)
+
+elif menu_selection == "Colegios":
+    # Obtener los nombres de los colegios
+    schools_data = fetch_schools()
+    school_names = schools_data['name'].tolist()
+    years_data = fetch_years()
+    years_names = years_data['name'].tolist()
+
+    # Crear columnas para los selectores
+    col1, col2 = st.columns(2)
+
+    # Selector de colegios en la primera columna
+    with col1:
+        selected_school = st.selectbox("Selecciona un colegio", school_names)
+
+    # Selector de años en la segunda columna
+    with col2:
+        selected_year = st.selectbox("Selecciona un año", years_names)
+
+    # Mostrar información del colegio seleccionado
+    if selected_school and selected_year:
+        # Obtener cursos del colegio seleccionado y el año
+        courses_data = fetch_courses_of_school(selected_school, selected_year)
+
+        # Mostrar cursos como selector
+        if not courses_data.empty:
+            # Obtener los nombres de los cursos para el selector
+            courses_list = courses_data['grade_name'] + " - " + courses_data['subject_name']
+            selected_course = st.selectbox("Selecciona un curso", courses_list)
+
+            # Obtener el ID del curso seleccionado
+            selected_course_id = courses_data[courses_list == selected_course]['course_id'].values[0]
+
+            # Mostrar información del curso seleccionado
+            if selected_course_id is not None:
+                course_info = courses_data[courses_data['course_id'] == selected_course_id]
+                if not course_info.empty:
+
+                    # Filtro de rol para los usuarios de este curso
+                    role_filter = st.selectbox("Selecciona el rol de los usuarios", ["Todos", "Profesores", "Alumnos"])
+
+                    # Filtra los datos basándote en el rol seleccionado
+                    user_data = fetch_users_by_course(selected_course_id)
+
+                    # Filtrar las columnas que deseas mostrar según el rol seleccionado
+                    if role_filter == "Profesores":
+                        columns_to_display = ["name", "lastName", "phone", "establishment_name", "career_name"]
+                        user_data = user_data[user_data['user_course_role'] == 'teacher']
+                    elif role_filter == "Alumnos":
+                        columns_to_display = ["name", "lastName", "phone", "emergencyName", "emergencyNumber", "emergencyRelationship"]
+                    else:  # Para "Todos"
+                        columns_to_display = ["name", "lastName", "phone"]
+
+                    # Seleccionar solo las columnas que deseas mostrar en el DataFrame
+                    filtered_data_display = user_data[columns_to_display].copy()
+
+                    # Convertir la columna birthday a datetime y luego formatearla
+                    # filtered_data_display['birthday'] = pd.to_datetime(filtered_data_display['birthday']).dt.date
+
+                    # Ajustar el ancho de las columnas
+                    column_widths = [100] * len(columns_to_display)  # Ancho fijo para todas las columnas
+
+                    # Mostrar la tabla con ancho de columnas ajustado
+                    st.dataframe(filtered_data_display.style.set_properties(**{'width': f'{column_widths[0]}px'}), use_container_width=True)
+
+                    # Llamada a la función que muestra los gráficos de usuarios para este curso
+                    display_user_charts(user_data)
+                    if role_filter == "Profesores":
+                        # Llamada a la función que muestra la distribución de universidades y carreras
+                        display_user_education(user_data)
+        else:
+            st.write("No hay cursos disponibles para este colegio y año.")
--- queries.py
+++ queries.py
@@ -30,21 +30,64 @@ def fetch_users():
     """
     with get_connection() as conn:
         return pd.read_sql(query, conn)
-
-# Consultar información de asistencia
-def fetch_attendance():
-    query = "SELECT * FROM asistencia"
+    
+    # Consultar información de colegios
+def fetch_schools():
+    query = """
+    SELECT name FROM "Schools"
+    """
+    with get_connection() as conn:
+        return pd.read_sql(query, conn)
+    
+def fetch_years():
+    query = """
+    SELECT name FROM "Years"
+    """
     with get_connection() as conn:
         return pd.read_sql(query, conn)
 
-# Consultar información de evaluaciones
-def fetch_evaluations():
-    query = "SELECT * FROM evaluaciones"
+def fetch_courses_of_school(school_name, year_name):
+    query = f"""
+    SELECT 
+        c.id AS course_id,
+        g.name AS grade_name,
+        sub.name AS subject_name
+
+    FROM "Courses" c
+    LEFT JOIN "Schools" s ON c."SchoolId" = s.id
+    LEFT JOIN "Years" y ON c."YearId" = y.id
+    LEFT JOIN "Grades" g ON c."GradeId" = g.id
+    LEFT JOIN "Subjects" sub ON c."SubjectId" = sub.id
+    WHERE s.name = '{school_name}' AND y.name = '{year_name}'
+    ORDER BY g.name, sub.name
+    """
     with get_connection() as conn:
         return pd.read_sql(query, conn)
 
-# Consultar otra información
-def fetch_other_data():
-    query = "SELECT * FROM otros"
+# Consultar información de usuarios por curso
+def fetch_users_by_course(course_id):
+    query = '''
+    SELECT 
+        u.id, 
+        u.name, 
+        u."lastName", 
+        u.rut, 
+        u.email, 
+        DATE(u.birthday) AS birthday, 
+        u.phone, 
+        u."emergencyName", 
+        u."emergencyNumber", 
+        u."emergencyRelationship", 
+        u."validEmail", 
+        u."termsAccepted", 
+        e.name AS establishment_name, 
+        c.name AS career_name, 
+        uc.role AS user_course_role 
+    FROM "Users" u 
+    LEFT JOIN "UserCourses" uc ON u.id = uc."UserId" 
+    LEFT JOIN "Establishments" e ON u."EstablishmentId" = e.id 
+    LEFT JOIN "Careers" c ON u."CareerId" = c.id 
+    WHERE uc."CourseId" = %s
+'''
     with get_connection() as conn:
-        return pd.read_sql(query, conn)
+        return pd.read_sql(query, conn, params=(int(course_id),))
